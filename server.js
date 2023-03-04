e95489fd1cbce978437e945e91c6030b1ad03bbb
sa8tyajeet46 <satyajeettripathy677@gmail.com>
2023-03-04 18:17:07 +0530

    jjn

--- server.js
+++ server.js
@@ -2,10 +2,6 @@ const app = require("./app.js");
 const dotenv = require("dotenv");
 const connectDatabase = require("./databaseConnection.js");
 const cloudinary = require("cloudinary").v2;
-process.on("uncaughtException", (err) => {
-  console.log(err.message);
-  process.exit(1);
-});
 
 connectDatabase();
 cloudinary.config({
@@ -17,10 +13,4 @@ cloudinary.config({
 });
 const server = app.listen(process.env.PORT, () => {
   console.log(`The server is running at ${process.env.PORT}`);
-});
-
-process.on("unhandledRejection", (err) => {
-  console.log(err.message);
-  console.log("The server is closing due to unhandled rejection");
-  server.close(() => process.exit(1));
 });
